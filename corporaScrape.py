9a6d1b070c6fd943d374cb51bd8f5eda3b4ba035
HapaxHypatia <r.haskmann@gmail.com>
2024-10-15 23:19:00 +1000

    Added duplicate checks

--- corporaScrape.py
+++ corporaScrape.py
@@ -45,8 +45,8 @@ def DBaddText(text):
 	val = (text["title"], authorID[0][0])
 	CHECKsql = "SELECT * FROM texts WHERE title = %s AND authorID = %s"
 	mycursor.execute(CHECKsql, val)
-
-	if not mycursor.fetchall():
+	searchresult = mycursor.fetchall()
+	if not searchresult:
 		print("Adding text: {} by {}".format(text['title'], text['author']))
 		add_text = ("INSERT INTO texts "
 				   "(title, textLength, textString, authorID) "
@@ -54,6 +54,22 @@ def DBaddText(text):
 		mycursor.execute(add_text, values)
 		mydb.commit()
 		print("Text added.")
+	else:
+		textID = searchresult[0][0]
+		check_length = "SELECT textLength FROM texts WHERE textID=%s"
+		mycursor.execute(check_length, (textID,))
+		current_length = mycursor.fetchall()
+		if current_length[0][0] != text['length']:
+			values = {
+				'length': text["length"],
+				'text': text["fulltext"],
+				'id': textID
+			}
+			update_text = ("UPDATE texts SET textLength=%s, textString=%s WHERE textID = %s ) "
+					   "VALUES (%(length)s, %(text)s, %(id)s)")
+			mycursor.execute(update_text, values)
+			mydb.commit()
+			print("Text updated.")
 
 
 driver = webdriver.Chrome()
@@ -68,7 +84,7 @@ authorElements = driver.find_elements(By.CSS_SELECTOR, value='.authors li a')
 for item in authorElements:
 	name = item.find_element(By.CSS_SELECTOR, value='span b').text
 	fullname = item.find_element(By.CSS_SELECTOR, value='span').text
-	DBaddAuthor(name, fullname)
+	# DBaddAuthor(name, fullname)
 author_links = [x.get_attribute("href") for x in authorElements]
 for author in author_links:
 	driver.get(author)
@@ -89,6 +105,7 @@ for author in author_links:
 		textdata["author"] = driver.find_element(By.ID, value='author').text
 		textdata["title"] = driver.find_element(By.ID, value='work').text
 		tablecells = driver.find_elements(by=By.TAG_NAME, value='td')
+		# TODO click next button until full text is gathered
 		textdata["fulltext"] = " ".join(cell.text for cell in tablecells)
 		textdata["length"] = len(textdata["fulltext"].split(" "))
 		texts.append(textdata)
